23c9a56c6dc4f82dcaa15d1d4a5fe6494826e819
AndreyGladikov <39302781+AndreyGladikov@users.noreply.github.com>
2018-07-25 18:45:09 +0300

    OrignCode

---
+++ PatternsSolution/Patterns/PatternsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Patterns.Pages;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Drawing.Imaging;
+
+namespace Patterns
+{
+    [TestClass]
+    public class PatternsTests
+    {
+        private static IWebDriver driver;
+
+        [TestInitialize]
+        public void initialize()
+        {
+            driver = new ChromeDriver(@"..\..\..\Patterns\Drivers\"); 
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void MailLogin()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.Login("AutoTest92", "Inq2020327");
+            HomePage homePage = new HomePage(driver);
+            Assert.IsTrue(homePage.getText().Contains(HomePage.UName), "You did not loged in ");
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void MailLogOut()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.Login("AutoTest92", "Inq2020327");
+            HomePage homePage = new HomePage(driver);
+            homePage.LogOut();
+            Assert.IsTrue(loginPage.Check(), "You are not looged out");
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+    }
+}
