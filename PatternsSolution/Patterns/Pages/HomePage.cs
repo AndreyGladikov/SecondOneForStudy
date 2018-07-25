a7af2c728eef8712ea5b60d6727314266be0dabe
AndreyGladikov <39302781+AndreyGladikov@users.noreply.github.com>
2018-07-25 18:36:00 +0300

    Revert "Revert "New Repo For Patters""
    This reverts commit f103fadd4f671462b9368a0b3ecbad267579a922.

---
+++ PatternsSolution/Patterns/Pages/HomePage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Patterns.Pages
+{
+    class HomePage
+    {
+        public const string UName = "Test Auto";
+
+        [FindsBy(How = How.XPath, Using = "//span[@class='uname']")]
+        private IWebElement UserName;
+        [FindsBy(How = How.XPath, Using = "//a[@class='button wide auth__reg']")]
+        private IWebElement LogOutBTN { get; set; }
+
+        public HomePage(IWebDriver driver)
+        {
+            //this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+        
+        public void LogOut()
+        {
+            UserName.Click();
+            LogOutBTN.Click();
+        }
+
+        public string getText()
+        {
+           return UserName.Text;
+        }
+    }
+}
